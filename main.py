13c5b85144d9b7e240d7fef2f48abfc92a532878
Kayden Drake Adams <kayden.adams@colorado.edu>
2021-04-30 00:56:47 +0000

    Improve formatting

--- main.py
+++ main.py
@@ -24,33 +24,33 @@ def shuffle():
 
 @client.event
 async def on_ready():
-    with open("tarot-cards/tarot-images.json") as file:
-      raw_cards = file.read()
+  with open("tarot-cards/tarot-images.json") as file:
+    raw_cards = file.read()
 
-    global cards
-    cards = json.loads(raw_cards)["cards"]
-    global deck
-    deck = cards.copy()
+  global cards
+  cards = json.loads(raw_cards)["cards"]
+  global deck
+  deck = cards.copy()
 
-    global card_images
-    card_images = {card_img: discord.File("tarot-cards/cards/" + card_img, filename=card_img) for card_img in os.listdir("tarot-cards/cards")}
-    print('We have logged in as {0.user}'.format(client))
+  global card_images
+  card_images = {card_img: discord.File("tarot-cards/cards/" + card_img, filename=card_img) for card_img in os.listdir("tarot-cards/cards")}
+  print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
-    # Return if the messsage is from the bot
-    if message.author == client.user:
-        return
+  # Return if the messsage is from the bot
+  if message.author == client.user:
+    return
 
-    if message.content.startswith('!draw'):
-        card = draw()
+  if message.content.startswith('!draw'):
+    card = draw()
 
-        file = card_images[card["img"]]
-        embed = discord.Embed()
-        img_url = "attachment://" + card["img"]
-        embed.set_image(url= img_url)
+    file = card_images[card["img"]]
+    embed = discord.Embed()
+    img_url = "attachment://" + card["img"]
+    embed.set_image(url = img_url)
 
-        await message.channel.send(content=card["name"],file=file, embed=embed)
+    await message.channel.send(content=card["name"],file=file, embed=embed)
 
 
 client.run(os.environ['BOT_TOKEN'])
